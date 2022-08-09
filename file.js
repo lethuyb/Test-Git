3744398983822d264059af13cb988479dbbc2a9a
lethuyb <l.thuy@mayass.com>
2022-08-09 14:48:05 +0900

    check file

--- file.js
+++ file.js
@@ -35,3 +35,70 @@ function AddName(name) {
     DataTransferItemList.DataTransferItem();
     encodeURI.apply(name);
 }
+function dragstart_handler(ev) {
+    console.log('dragStart');
+    // Add this element's id to the drag payload so the drop handler will
+    // know which element to add to its tree
+    const dataList = ev.dataTransfer.items;
+    dataList.add(ev.target.id, 'text/plain');
+    // Add some other items to the drag payload
+    dataList.add('<p>Paragraph…</p>', 'text/html');
+    dataList.add('http://www.example.org','text/uri-list');
+  }
+  
+  function drop_handler(ev) {
+    console.log('Drop');
+    ev.preventDefault();
+    const data = event.dataTransfer.items;
+    // Loop through the dropped items and log their data
+    for (const item of data) {
+      if ((item.kind === 'string') &&
+          (item.type.match('^text/plain'))) {
+        // This item is the target node
+        item.getAsString((s) => {
+          ev.target.appendChild(document.getElementById(s));
+        });
+      } else if ((item.kind === 'string') &&
+                 (item.type.match('^text/html'))) {
+        // Drag data item is HTML
+        item.getAsString((s) => {
+          console.log(`… Drop: HTML = ${s}`);
+        });
+      } else if ((item.kind === 'string') &&
+                 (item.type.match('^text/uri-list'))) {
+        // Drag data item is URI
+        item.getAsString((s) => {
+          console.log(`… Drop: URI = ${s}`);
+        });
+      }
+    }
+  }
+  
+  function dragover_handler(ev) {
+    console.log('dragOver');
+    ev.preventDefault();
+    // Set the dropEffect to move
+    ev.dataTransfer.dropEffect = 'move'
+  }
+  
+  function dragend_handler(ev) {
+    console.log('dragEnd');
+    const dataList = ev.dataTransfer.items;
+    // Clear all the files. Iterate in reverse order to safely remove.
+    for (let i = dataList.length - 1; i >= 0; i--) {
+      if (dataList[i].kind === 'file') {
+        dataList.remove(i);
+      }
+    }
+    // Clear any remaining drag data
+    dataList.clear();
+  }
+  
+  const source = document.querySelector('#source');
+  source.addEventListener('dragstart', dragstart_handler);
+  source.addEventListener('dragend', dragend_handler);
+  
+  const target = document.querySelector('#target');
+  target.addEventListener('drop', drop_handler);
+  target.addEventListener('dragover', dragover_handler);
+  
